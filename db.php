e8739656e3af762f3998e5ae43c953a1f9bcd9f5
MsrParacetamol <alcala.ma.maam@gmail.com>
2025-11-28 03:56:09 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "127.0.0.1"; // o "localhost"
-$user = "root";
-$pass = "1029"; // sin contraseña por defecto
+$user = "huntech";
+$pass = "1029";
 $dbname = "huntechdb";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
@@ -10,4 +10,3 @@ if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 ?>
-

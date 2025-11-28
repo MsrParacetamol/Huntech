be9105956dca4f11a52b68478df0788fce8e965e
MsrParacetamol <alcala.ma.maam@gmail.com>
2025-11-28 02:57:19 -0600

    Update database password in db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = getenv("DB_HOST") ?: "huntech-db";
 $user = getenv("DB_USER") ?: "root";
-$pass = getenv("DB_PASSWORD") ?: "tu_contraseña";
+$pass = getenv("DB_PASSWORD") ?: "1029";
 $dbname = getenv("DB_NAME") ?: "huntechdb";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
@@ -10,3 +10,4 @@ if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 ?>
+

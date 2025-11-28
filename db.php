4322ac0825d0427c2f5e10028ace733c2a1da582
MsrParacetamol <alcala.ma.maam@gmail.com>
2025-11-28 03:49:44 -0600

    Update database password in db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "127.0.0.1"; // o "localhost"
 $user = "root";
-$pass = ""; // sin contraseña por defecto
+$pass = "1029"; // sin contraseña por defecto
 $dbname = "huntechdb";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
@@ -10,3 +10,4 @@ if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 ?>
+
